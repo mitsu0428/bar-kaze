20b2d63d8f4762ce05779b39d02d8e41cc30b0c9
mitsu0428 <mitsuprograming@gmail.com>
2023-07-06 17:39:32 +0900

    fix: 改行は共通して使える様に外に切り出す

---
+++ src/components/ui/StyledBr.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledBr = () => {
+  return <Br />;
+};
+
+export default StyledBr;
+
+const Br = styled.br`
+  padding: 8px;
+`;
